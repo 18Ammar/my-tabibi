53fa3acb9c1fb2d2f86b2bccdffcd86101349ff3
Ammar <Ammar@gmail.com>
2024-01-21 13:39:37 +0300

    -----

--- myapp/dl_model_implemention/health_chat_ai.py
+++ myapp/dl_model_implemention/health_chat_ai.py
@@ -2,18 +2,37 @@ import tensorflow as tf
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.models import Sequential, Model
-from tensorflow.keras.layers import Dense, LSTM, Embedding, Input, Flatten
+from tensorflow.keras.layers import Dense, LSTM, Embedding, Input, Flatten,Bidirectional,Dropout
 import numpy as np
 import json
 import string
 import pandas as pd
-from sklearn.preprocessing import LabelEncoder
 import random
 
-# Prepare the dataset
+# Load dataset from JSON file
 filePath = "D:\\health_AI\\myapp\\dl_model_implemention\\dataset.json"
 
-# Load dataset from JSON file
+
+
+def load_glove_embeddings(file_path, word_index, vocab_size, embedding_dim):
+    embeddings_index = {}
+    with open(file_path, encoding='utf-8') as f:
+        for line in f:
+            values = line.split()
+            word = values[0]
+            coefs = np.asarray(values[1:], dtype='float32')
+            embeddings_index[word] = coefs
+
+    embedding_matrix = np.zeros((vocab_size, embedding_dim))
+    for word, i in word_index.items():
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None:
+            embedding_matrix[i] = embedding_vector
+
+    return embedding_matrix
+
+
+
 with open(filePath) as f:
     dataset = json.load(f)
 
@@ -27,50 +46,44 @@ for intent in dataset["intents"]:
         questionList.append(qu)
         tags.append(intent['tag'])
 
-# Create a DataFrame from Data set
+# Create a DataFrame from the dataset
 data = pd.DataFrame({"question": questionList, "tags": tags})
 
 # Preprocess the text data
 data['question'] = data['question'].apply(lambda wrd: [itrs.lower() for itrs in wrd if itrs not in string.punctuation])
 data['question'] = data['question'].apply(lambda wrd: ''.join(wrd))
 
-# Tokenize the text 
-tokenizer = Tokenizer(num_words=2000)
-tokenizer.fit_on_texts(data['question']) 
+# Tokenize the text
+tokenizer = Tokenizer(num_words=5000)
+tokenizer.fit_on_texts(data['question'])
 
-train_data, val_data = train_test_split(data, test_size=0.2, random_state=42)
 
-x_train = pad_sequences(tokenizer.texts_to_sequences(train_data['question']))
-x_val = pad_sequences(tokenizer.texts_to_sequences(val_data['question']))
+x_train = pad_sequences(tokenizer.texts_to_sequences(data['question']))
 
-y_train = le.transform(train_data['tags'])
-y_val = le.transform(val_data['tags'])
+# Use StringLookup to encode labels
+tag_lookup = tf.keras.layers.StringLookup(vocabulary=np.unique(data['tags']), num_oov_indices=0, mask_token=None)
+
+y_train = tag_lookup(data['tags'])
 
 vocab_size = len(tokenizer.word_index) + 1
 print("Number of unique words:", vocab_size)
 
 output_len = len(np.unique(y_train))
 print("Output length:", output_len)
+embedding_matrix = load_glove_embeddings('D:\\health_AI\\myapp\\dl_model_implemention\\glove.6B.100d.txt', tokenizer.word_index, vocab_size, embedding_dim=100)
 
-train_data, val_data = train_test_split(data, test_size=0.2, random_state=42)
-
-x_train = pad_sequences(tokenizer.texts_to_sequences(train_data['question']))
-x_val = pad_sequences(tokenizer.texts_to_sequences(val_data['question']))
-
-y_val = le.transform(val_data['tags'])
-
-# Define the model
 i = Input(shape=(x_train.shape[1],))
-x = Embedding(vocab_size, 10)(i)
-x = LSTM(10, return_sequences=True)(x)
+x = Embedding(vocab_size, 100, weights=[embedding_matrix], input_length=x_train.shape[1], trainable=False)(i)
+x = Bidirectional(LSTM(20, return_sequences=True))(x)
+x = Dropout(0.2)(x)
 x = Flatten()(x)
 x = Dense(output_len, activation="softmax")(x)
 model = Model(i, x)
-
 model.compile(loss="sparse_categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
 
-# Train the model
-history = model.fit(x_train, y_train, validation_data=(x_val, y_val), epochs=200)
+
+history = model.fit(x_train, y_train,  epochs=10)
+model.summary()
 
 while True:
     text_p = []
@@ -90,9 +103,10 @@ while True:
 
     output = model.predict(predicate_input)
     output = np.argmax(output)
-    
-    answer_tag = le.inverse_transform([output])[0]
+
+    # Convert the output to the corresponding string label
+    answer_tag = tag_lookup.get_vocabulary()[output]
     
     respons = random.choice(answerList[answer_tag])
-    
+
     print("Bot:", respons)
